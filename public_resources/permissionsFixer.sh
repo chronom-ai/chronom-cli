82ea3ed33e2624a04ad40acf568af7ac70dc6a8e
sagi-chronom <sagi@chronom.ai>
2024-01-05 23:50:15 +0200

    Added fixed policy + fixed script

--- public_resources/permissionsFixer.sh
+++ public_resources/permissionsFixer.sh
@@ -77,6 +77,10 @@
 
 json=$(cat readonly-policy-new.json)
 
+# Empty Policy JSON
+emptyPolicyJson='{ "Version": "2012-10-17", "Statement": [ { "Sid": "ReadOnlyActions", "Effect": "Allow", "Action": [], "Resource": "*" } ] }'
+
+
 # Get the actions from the JSON
 actions=$(echo $json | jq -r '.Statement[].Action[]')
 
@@ -143,9 +147,133 @@ newActions=($(echo "${newActions[@]}" | tr ' ' '\n' | sort -u | tr '\n' ' '))
 # Sort the newActions array
 newActions=($(echo "${newActions[@]}" | tr ' ' '\n' | sort | tr '\n' ' '))
 
-# Create a new JSON object with the newActions array
-# # newJson=$(echo $json | jq --argjson newActions "${newActions[@]}" '.Statement[].Action = $newActions')
-newJson=$(echo "$json" | jq --argjson newActions "$(printf '%s\n' "${newActions[@]}" | jq -R . | jq -s .)" '.Statement[].Action = $newActions')
+# The actions are structured as <service>:<action>
+# We need to create a json object that looks like this:
+# [
+#   {
+#     "service1": 56, # Number of chars in the actions array without whitespace
+#     "actions": [
+#       "service1:action1",
+#       "service1:action2",
+#       "service1:action3"
+#     ]
+#   },
+#   {
+#     "service2": 56, # Number of chars in the actions array without whitespace
+#     "actions": [
+#       "service2:action1",
+#       "service2:action2",
+#       "service2:action3"
+#     ]
+#   }
+# ]
 
-# Print the new JSON object
-echo $newJson > readonly-policy-new-wildcard-better.json
+# Create an empty array to store the services in
+services=()
+
+# Loop through the actions
+for newAction in "${newActions[@]}"
+do
+  # Split the action after the first colon
+  service=$(echo $newAction | awk -F':' '{print $1}')
+  # Add the service to the services array
+  services+=($service)
+done
+# Remove duplicates from the services array
+services=($(echo "${services[@]}" | tr ' ' '\n' | sort -u | tr '\n' ' '))
+
+# Create an empty array to store the JSON objects in
+jsonObjects=()
+
+# Loop through the services
+for service in "${services[@]}"
+do
+  # Create an empty array to store the actions in
+  actions=()
+  # Loop through the actions
+  for newAction in "${newActions[@]}"
+  do
+    # Split the action after the first colon
+    actionService=$(echo $newAction | awk -F':' '{print $1}')
+    # Check if the action contains the service
+    if [[ $actionService == $service ]]
+    then
+      # If it does, add it to the actions array
+      actions+=($newAction)
+    fi
+  done
+  # Count the number of characters in the actions array without whitespace
+  actionsLength=$(echo "${actions[@]}" | tr -d '[:space:]' | wc -c)
+  # Create a JSON object with the service and actions
+  jsonObject=$(jq -n --arg service "$service" --arg count "$actionsLength" --argjson actions "$(printf '%s\n' "${actions[@]}" | jq -R . | jq -s .)" '{"service": $service, "count": $count, "actions": $actions}')
+
+  echo $service
+
+  # Add the JSON object to the jsonObjects array
+  jsonObjects+=($jsonObject)
+done
+
+# create a new AWS IAM Policy with the new actions, with up to 6000 characters per policy actions (use the count property to determine the length of the actions array)
+# do not break up the actions array for a single service into multiple policies
+# each policy should have a maximum of 6000 characters in the actions array
+# Do not change the order of the actions array, preffer to have less then 6000 characters in the actions array instead of changing the order of the actions array
+
+# Convert the $jsonObjects array to a JSON object
+convertedJsonObjects=$(printf '%s\n' "${jsonObjects[@]}" | jq -s .)
+
+echo $convertedJsonObjects > convertedJsonObjects.json
+
+
+tempCounter=0
+countedActions=()
+policyCounter=0
+while [ "$tempCounter" -lt 5000 ] && [ "$convertedJsonObjects" != "[]" ]
+do
+  # Get the first JSON object from the $convertedJsonObjects array
+  jsonObject=$(echo $convertedJsonObjects | jq -r '.[0]')
+  # Get the service from the JSON object
+  service=$(echo $jsonObject | jq -r '.service')
+  # Get the actions from the JSON object
+  actions=$(echo $jsonObject | jq -r '.actions[]')
+  # Get the count from the JSON object
+  count=$(echo $jsonObject | jq -r '.count')
+  tempCounter=$((tempCounter + count))
+  if [[ $tempCounter -lt 5000 ]]
+  then
+    # Add the actions to the countedActions array
+    echo "Service: $service added to policy # $policyCounter"
+    countedActions+=($actions)
+    # if service equals eks then add"eks:AccessKubernetesApi" to the countedActions array
+    if [[ $service == "eks" ]]
+    then
+      countedActions+=("eks:AccessKubernetesApi")
+    fi
+
+    # Remove the first JSON object from the $convertedJsonObjects array
+    convertedJsonObjects=$(echo $convertedJsonObjects | jq -r '.[1:]')
+  else
+    echo "Service: $service not added to policy # $policyCounter"
+    # Create a new JSON object with the countedActions array
+    newJson=$(echo "$emptyPolicyJson" | jq --argjson countedActions "$(printf '%s\n' "${countedActions[@]}" | jq -R . | jq -s .)" '.Statement[].Action = $countedActions')
+    # Print the new JSON object
+    echo $newJson > readonly-policy-part$policyCounter.json
+
+    echo "Created policy # $policyCounter"
+
+    policyCounter=$((policyCounter + 1))
+    # Empty the countedActions array
+    countedActions=()
+    # Empty the tempCounter
+    tempCounter=0
+  fi
+done
+
+# Check if there are remaining actions in countedActions
+if [ ${#countedActions[@]} -ne 0 ]; then
+  # Create a new JSON object with the countedActions array
+  newJson=$(echo "$emptyPolicyJson" | jq --argjson countedActions "$(printf '%s\n' "${countedActions[@]}" | jq -R . | jq -s .)" '.Statement[].Action = $countedActions')
+  # Print the new JSON object
+  echo $newJson > readonly-policy-part$policyCounter.json
+
+  echo "Created policy # $policyCounter"
+fi
